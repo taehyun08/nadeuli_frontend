a7b4d3e8f3772ed5507146a67239db8c3c716504
taehyun08 <didls1643@naver.com>
2023-12-07 15:59:41 +0900

    Style : 가독성을 위한 코드 개선
    한 라우팅에서 여러줄의 코드를 한줄로 합침

--- src/App.js
+++ src/App.js
@@ -41,136 +41,51 @@ import DeleteDeliveryNotification from "./page/nadeuli_delivery/DeleteDeliveryNo
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/nadeuliPay/getNadeuliPayList/:currentPage/:tradeType/:tag"
-          element={<NadeuliPayList />}
-        />
-        <Route
-          path="/nadeuliPay/nadeuliPayCharge"
-          element={<NadeuliPayCharge />}
-        />
-        <Route
-          path="/nadeuliPay/nadeuliPayWithdraw"
-          element={<NadeuliPayWithdraw />}
-        />
-        <Route path="/nadeuliPay/nadeuliPayPay" element={<NadeuliPayPay />} />
+  <Router>
+    <Routes>
+      {/* NadeuliPay Routes */}
+      <Route path="/nadeuliPay/getNadeuliPayList/:currentPage/:tradeType/:tag" element={<NadeuliPayList />} />
+      <Route path="/nadeuliPay/nadeuliPayCharge" element={<NadeuliPayCharge />} />
+      <Route path="/nadeuliPay/nadeuliPayWithdraw" element={<NadeuliPayWithdraw />} />
+      <Route path="/nadeuliPay/nadeuliPayPay" element={<NadeuliPayPay />} />
 
-        <Route
-          path="/product/home/:currentPage/:gu"
-          element={<ProductList />}
-        />
-        <Route
-          path="/product/getProduct/:productId"
-          element={<ProductDetail />}
-        />
-        <Route path="/product/updateProduct" element={<ProductUpdate />} />
-        <Route path="/product/addProduct" element={<ProductAdd />} />
-        <Route
-          path="/product/getMyProductList/:tag/:currentPage"
-          element={<MyProductList />}
-        />
-        <Route
-          path="/product/deleteProduct/:productId"
-          element={<ProductDelete />}
-        />
-        <Route path="/product/saleCompleted" element={<SaleCompleted />} />
+      {/* Product Routes */}
+      <Route path="/product/home/:currentPage/:gu" element={<ProductList />} />
+      <Route path="/product/getProduct/:productId" element={<ProductDetail />} />
+      <Route path="/product/updateProduct" element={<ProductUpdate />} />
+      <Route path="/product/addProduct" element={<ProductAdd />} />
+      <Route path="/product/getMyProductList/:tag/:currentPage" element={<MyProductList />} />
+      <Route path="/product/deleteProduct/:productId" element={<ProductDelete />} />
+      <Route path="/product/saleCompleted" element={<SaleCompleted />} />
 
-        <Route path="/trade/addTradeReview" element={<TradeReviewAdd />} />
-        <Route
-          path="/trade/updateTradeReview"
-          element={<TradeReviewUpdate />}
-        />
-        <Route
-          path="/trade/getTradeReviewList/:tag/:currentPage"
-          element={<TradeReviewList />}
-        />
-        <Route
-          path="/trade/deleteTradeReview/:tradeReviewId"
-          element={<TradeReviewDelete />}
-        />
-        <Route
-          path="/trade/addTradingSchedule"
-          element={<TradeScheduleAdd />}
-        />
-        <Route
-          path="/trade/getTradingSchedule/:tradeScheduleId"
-          element={<TradeScheduleDetail />}
-        />
-        <Route
-          path="/trade/updateTradingSchedule"
-          element={<TradeScheduleUpdate />}
-        />
-        <Route
-          path="/trade/getTradingScheduleList/:tag/:currentPage"
-          element={<TradeScheduleList />}
-        />
+      {/* Trade Routes */}
+      <Route path="/trade/addTradeReview" element={<TradeReviewAdd />} />
+      <Route path="/trade/updateTradeReview" element={<TradeReviewUpdate />} />
+      <Route path="/trade/getTradeReviewList/:tag/:currentPage" element={<TradeReviewList />} />
+      <Route path="/trade/deleteTradeReview/:tradeReviewId" element={<TradeReviewDelete />} />
+      <Route path="/trade/addTradingSchedule" element={<TradeScheduleAdd />} />
+      <Route path="/trade/getTradingSchedule/:tradeScheduleId" element={<TradeScheduleDetail />} />
+      <Route path="/trade/updateTradingSchedule" element={<TradeScheduleUpdate />} />
+      <Route path="/trade/getTradingScheduleList/:tag/:currentPage" element={<TradeScheduleList />} />
 
-        {/* 다른 라우트에 맞게 컴포넌트와 경로를 추가해주세요 */}
-
-        <Route
-          path="/nadeulidelivery/getAddOrUpdateUsedDeliveryOrder/:tag"
-          element={<GetAddOrUpdateUsedDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/addDeliveryOrder"
-          element={<AddDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/updateDeliveryOrder"
-          element={<UpdateDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/getDeliveryOrder/:nadeuliDeliveryId"
-          element={<GetDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/getDeliveryOrderList"
-          element={<GetDeliveryOrderList />}
-        />
-        <Route
-          path="/nadeulidelivery/getMyOrderHistoryList"
-          element={<GetMyOrderHistoryList />}
-        />
-        <Route
-          path="/nadeulidelivery/getMyDeliveryHistoryList"
-          element={<GetMyDeliveryHistoryList />}
-        />
-        <Route
-          path="/nadeulidelivery/getMyAcceptedDeliveryHistoryList"
-          element={<GetMyAcceptedDeliveryHistoryList />}
-        />
-        <Route
-          path="/nadeulidelivery/cancelDeliveryOrder/:nadeuliDeliveryId"
-          element={<CancelDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/acceptDeliveryOrder"
-          element={<AcceptDeliveryOrder />}
-        />
-        <Route
-          path="/nadeulidelivery/cancelDelivery/:nadeuliDeliveryId"
-          element={<CancelDelivery />}
-        />
-        <Route
-          path="/nadeulidelivery/completeDelivery/:nadeuliDeliveryId"
-          element={<CompleteDelivery />}
-        />
-        <Route
-          path="/nadeulidelivery/getDeliveryNotificationList"
-          element={<GetDeliveryNotificationList />}
-        />
-        <Route
-          path="/nadeulidelivery/updateIsRead/:deliveryNotificationId"
-          element={<UpdateIsRead />}
-        />
-        <Route
-          path="/nadeulidelivery/deleteDeliveryNotification/:deliveryNotificationId"
-          element={<DeleteDeliveryNotification />}
-        />
-      </Routes>
-    </Router>
+      {/* NadeuliDelivery Routes */}
+      <Route path="/nadeulidelivery/getAddOrUpdateUsedDeliveryOrder/:tag" element={<GetAddOrUpdateUsedDeliveryOrder />} />
+      <Route path="/nadeulidelivery/addDeliveryOrder" element={<AddDeliveryOrder />} />
+      <Route path="/nadeulidelivery/updateDeliveryOrder" element={<UpdateDeliveryOrder />} />
+      <Route path="/nadeulidelivery/getDeliveryOrder/:nadeuliDeliveryId" element={<GetDeliveryOrder />} />
+      <Route path="/nadeulidelivery/getDeliveryOrderList" element={<GetDeliveryOrderList />} />
+      <Route path="/nadeulidelivery/getMyOrderHistoryList" element={<GetMyOrderHistoryList />} />
+      <Route path="/nadeulidelivery/getMyDeliveryHistoryList" element={<GetMyDeliveryHistoryList />} />
+      <Route path="/nadeulidelivery/getMyAcceptedDeliveryHistoryList" element={<GetMyAcceptedDeliveryHistoryList />} />
+      <Route path="/nadeulidelivery/cancelDeliveryOrder/:nadeuliDeliveryId" element={<CancelDeliveryOrder />} />
+      <Route path="/nadeulidelivery/acceptDeliveryOrder" element={<AcceptDeliveryOrder />} />
+      <Route path="/nadeulidelivery/cancelDelivery/:nadeuliDeliveryId" element={<CancelDelivery />} />
+      <Route path="/nadeulidelivery/completeDelivery/:nadeuliDeliveryId" element={<CompleteDelivery />} />
+      <Route path="/nadeulidelivery/getDeliveryNotificationList" element={<GetDeliveryNotificationList />} />
+      <Route path="/nadeulidelivery/updateIsRead/:deliveryNotificationId" element={<UpdateIsRead />} />
+      <Route path="/nadeulidelivery/deleteDeliveryNotification/:deliveryNotificationId" element={<DeleteDeliveryNotification />} />
+    </Routes>
+  </Router>
   );
 }
 
