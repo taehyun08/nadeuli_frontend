0b1660adf1173373ecd7e7715943a07ed062f4a5
dhkimk96 <donghun.k96@gmail.com>
2023-12-21 11:26:29 +0900

    Feat : OrikkiriHome 추가, AddStreaming 추가

--- src/redux/modules/streaming.js
+++ src/redux/modules/streaming.js
@@ -1,14 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
-import axiosInstance from '../../util/axios';
+import { get, post } from "../../util/axios";
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // 스트리밍 채널 생성
-export const CreateliveChannel= (formData, navigate) => {
+export const createliveChannel= (formData, navigate) => {
   return async function (dispatch) {
     try {
-      const res = await axios.post(`${BASE_URL}/dongNe/addStreaming`, formData); // 헤더 제거
+      const res = await axios.post(`${BASE_URL}/dongNe/addChannel`, formData); // 헤더 제거
       console.log(res.data); 
       dispatch(addChannel(res.data));
     } catch (err) {
@@ -18,23 +18,43 @@ export const CreateliveChannel= (formData, navigate) => {
   };
 
 // 스트리밍 채널 조회
-export const GetChannelDetail = (channelId) => {
-  return async function (dispatch) {
-    try {
-      const res = await axiosInstance.get(`/dongNe/getStreaming/${channelId}`);
-      console.log(res.data);
-      dispatch(getChannel(res.data));
-    } catch (err) {
+export const getChannelDetail = (channelId) => {
+  return async function (dispatch, getState) {
+    await get(`/dongNe/getChannel/${channelId}`)
+    .then((res) => {
+      console.log(res);
+      dispatch(getChannel(res));
+      const currentState = getState();
+      console.log('get이후 Current state:', currentState);
+    })
+    .catch((err) => {
       console.log(err);
-    }
-  };
+    });
 };
+};  
+
+
+// // 스트리밍 리스트 조회
+// export const channelList = (channelId) => {
+//   return async function (dispatch, getState) {
+//       await get(`/dongNe/getChannel/${channelId}`)
+//       .then((res) => {
+//         console.log(res);
+//         dispatch(getChannelList(res));
+//         const currentState = getState();
+//         console.log('Current state:', currentState);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+// };
 
 // 스트리밍 채널 삭제
 export const removeChannel = (postId, navigate) => {
   return async function (dispatch) {
     console.log('동네 게시물 삭제 실행 중');
-    await axios.get(`${BASE_URL}/dongNe/deletePost/${postId}`)
+    await axios.get(`${BASE_URL}/dongNe/deleteChannel/${postId}`)
       .then((res) => {
         dispatch(deleteChannel(res.data));
         navigate("/dongNeHome");
@@ -46,21 +66,6 @@ export const removeChannel = (postId, navigate) => {
 };
 
 
-// // 동네 게시물 삭제
-// export const removeDongNePost = (postId, navigate) => {
-//   return async function (dispatch) {
-//     console.log('동네 게시물 삭제 실행 중');
-//     await axios.get(`${BASE_URL}/dongNe/deletePost/${postId}`)
-//       .then((res) => {
-//         dispatch(deleteDongNePost(res.data));
-//         navigate("/dongNeHome");
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
-// };
-
 // 리덕스슬라이스
 const liveChannelSlice = createSlice({
   name: 'channel',
@@ -70,8 +75,8 @@ const liveChannelSlice = createSlice({
   },
   reducers: {
     addChannel: (state, action) => {
-      // action.payload로 새로운 게시물을 추가할 수 있음
-      state.channelList.push(action.payload);
+      state.channel = action.payload;
+      // state.channelList.push(action.payload);
     },
     getChannelList: (state, action) => {
       // action.payload를 통해 서버로부터 받은 게시물 목록을 저장할 수 있음
@@ -87,6 +92,6 @@ const liveChannelSlice = createSlice({
   },
 });
 
-export const { addChannel, getChannelList, getChannel, deleteChannel} = liveChannelSlice.actions;
+export const { addChannel, getChannelList, getChannel, deleteChannel } = liveChannelSlice.actions;
 
 export default liveChannelSlice.reducer;
