0acf2d6024f277b545f9fca0780d8b5d6934064e
Coalman96 <mayuaa001@gmail.com>
2023-12-22 07:08:10 +0900

    Fix : 우리끼리 가입신청서 수정

--- src/pages/orikkiri/AddOrikkiriSignUp.js
+++ src/pages/orikkiri/AddOrikkiriSignUp.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
 import HeaderBack from '../../components/HeaderBack';
 import styled from 'styled-components';
-import { MDBBreadcrumb, MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow, MDBTextArea, MDBTypography, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBBreadcrumb, MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow, MDBTextArea, MDBTypography, MDBBtn, MDBCardImage } from 'mdb-react-ui-kit';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import { getOrikkiri } from '../../util/orikkiriManageAxios';
 
 const AddOrikkiriSignUp = () => {
     const BASE_URL = process.env.REACT_APP_BASE_URL;
     const navigate = useNavigate();
     const [ansQuestions, setAnsQuestions] = useState([]);
     const [answers, setAnswers] = useState([]); // 입력된 값을 저장할 상태
+    const [orikkiri, setOrikkiri] = useState(null);
     const params = useParams();
-    const { orikkiriId } = params.orikkiriId;
+    const orikkiriId = params.orikkiriId;
     const member = useSelector((state) => state.member);
 
     //질문 가져오는 함수
     useEffect(() => {
+
+        console.log(orikkiriId)
         const fetchAnsQuestions = async () => {
             try {
                 const response = await axios.get(`${BASE_URL}/orikkiriManage/getAnsQuestionList/${orikkiriId}`);
@@ -29,6 +33,24 @@ const AddOrikkiriSignUp = () => {
 
         fetchAnsQuestions();
     }, [orikkiriId]);
+
+    useEffect(() => {
+        // 오리끼리 정보를 받아오는 함수 호출
+        const fetchOrikkiri = async () => {
+            try {
+                // getOrikkiri 함수를 사용하여 서버에서 오리끼리 정보 받아오기
+                const response = await getOrikkiri(orikkiriId);
+                console.log(response.data)
+                // 받아온 오리끼리 정보를 상태에 저장
+                setOrikkiri(response.data);
+            } catch (error) {
+                console.error('Error fetching orikkiri:', error);
+            }
+        };
+
+        // 컴포넌트가 마운트될 때 오리끼리 정보를 받아오도록 설정
+        fetchOrikkiri();
+    }, []);  // 빈 배열을 두어 한 번만 호출하도록 설정
 
     // 답변 값 저장
     const handleAnswerChange = (e, index) => {
@@ -102,14 +124,21 @@ const AddOrikkiriSignUp = () => {
                                     className="mb-3"
                                     style={{ textAlign: 'left' }}
                                 >
-                                    <span class="badge badge-primary p-1">우리끼리 이름</span>
+                                    <span class="badge badge-primary p-1">{orikkiri?.orikkiriName}</span>
                                 </MDBTypography>
+                                <MDBCardImage
+                                    alt="picture"
+                                    src={orikkiri?.orikkiriPicture}
+                                    className="mb-5"
+                                    style={{ width: '150px', height: '150px', borderRadius: '8px' }}
+                                    fluid
+                                />
                                 <MDBTypography
                                     tag="h6"
                                     className="p-1"
                                     style={{ textAlign: 'left' }}
                                 >
-                                    [우리끼리 이름]에 가입신청을 위한 아래 질문에 답해주세요.
+                                    {orikkiri?.orikkiriName}에 가입신청을 위한 아래 질문에 답해주세요.
                                     <br />
                                     <p
                                         className="mt-2"
@@ -117,6 +146,14 @@ const AddOrikkiriSignUp = () => {
                                     >
                                         <strong>방장이 해당 신청을 승인하면 우리끼리에 가입됩니다.</strong>
                                     </p>
+                                </MDBTypography>
+                                <MDBTypography
+                                    tag="h6"
+                                    className="p-1"
+                                    style={{ textAlign: 'left' }}
+                                >
+                                    {orikkiri?.orikkiriIntroduction}
+                                    <br />
                                 </MDBTypography>
                                 <br />
                                 {ansQuestions.map((question, index) => (
